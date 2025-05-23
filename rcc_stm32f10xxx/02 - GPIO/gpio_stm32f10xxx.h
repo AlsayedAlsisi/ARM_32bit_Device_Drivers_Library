aabc41751de725a3e22714213c51ada592bfe1c8
Alsayed Alsisi <alsayed.alsisi@gmail.com>
2025-05-23 22:57:28 +0300

    Adding code skeleton for gpio device driver

---
+++ rcc_stm32f10xxx/02 - GPIO/gpio_stm32f10xxx.h
@@ -0,0 +1,59 @@
+/*********************************************************************************************************************
+* Author : Alsayed Alsisi
+* Date   : Saturday, November 07, 2020
+* Version: 1.0
+* Contact: alsayed.alsisi@gmail.com
+* License:
+* You have the right to use the file as you wish in any educational or commercial purposes under the following
+* conditions:
+* - This file is to be used as is. No modifications are to be made to any part of the file, including this section.
+* - This section is not to be removed under any circumstances.
+* - Parts of the file may be used separately under the condition they are not modified, and preceded by this section.
+* - Any bug encountered in this file or parts of it should be reported to the email address given above to be fixed.
+* - No warranty is expressed or implied by the publication or distribution of this source code.
+*********************************************************************************************************************/
+/*********************************************************************************************************************
+* File Information:
+** File Name:
+*  gpio_stm32f10xxx.h
+* 
+** Description:
+*  This file contains the public programming interfaces for the device driver of GPIO peripheral of the stm32f10xxx 
+*  family of microcontroller. This file can be used with any of the following members of this family:
+*  stm32f103xx, 
+*********************************************************************************************************************/
+
+
+/*********************************************************************************************************************
+                                               << Header Guard >>
+*********************************************************************************************************************/
+#ifndef GPIO_STM3210XXX_H_		
+#define GPIO_STM3210XXX_H_
+
+/*********************************************************************************************************************
+                                               << File Inclusions >>
+*********************************************************************************************************************/
+#include <stdint.h>
+
+
+/*********************************************************************************************************************
+                                               << Public Constants >>
+*********************************************************************************************************************/
+
+
+/*********************************************************************************************************************
+                                               << Public Data Types >>
+*********************************************************************************************************************/
+
+
+/*********************************************************************************************************************
+                                           << Public Function Declarations >>
+*********************************************************************************************************************/
+
+
+
+
+#endif /* GPIO_STM3210XXX_H_ */ 
+/*********************************************************************************************************************
+                                               << End of File >>
+*********************************************************************************************************************/
